c2791480f3bc92f012c8bb3b45642d02a21760ae
Yosuke Mizutani <mogproj@gmail.com>
2023-03-12 17:34:18 -0600

    Update colored logger

--- python/command-line/command_line_with_colored_log.py
+++ python/command-line/command_line_with_colored_log.py
@@ -23,37 +23,51 @@ except ImportError:
     sys.exit(1)
 
 
-LOG_LEVELS = {'none': 51, 'crit': 50, 'error': 40, 'warn': 30, 'info': 20, 'debug': 10}
-
 # paths
 SCRIPT_PATH = os.path.realpath(__file__)
 SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
 
+# Custom log levels.
+logging.TRACE = 5
+logging.SUCCESS = 60
+logging.NONE = 100
+LOG_LEVELS = {'success': logging.SUCCESS, 'crit': logging.CRITICAL, 'error': logging.ERROR, 'warn': logging.WARNING, 'info': logging.INFO, 'debug': logging.DEBUG, 'trace': logging.TRACE, 'none': logging.NONE, 'all': logging.TRACE}
 
-def get_logger(log_level=logging.CRITICAL + 1, colored=True):
+
+def get_logger(log_level=logging.NONE, colored=True):
     """Logger settings."""
 
-    class ColoredFormatter(logging.Formatter):
-        # ANSI ECMA-48 color codes (semicolon-separated):
-        # bold=1, half-bright=2, underscore=4, doubly-underlined=21, normal-intensity=22
-        # foreground:30+color, background:40+color
-        # colors: black=0, red=1, green=2, brown=3, blue=4, magenta=5, cyan=6, white=7
-        reset = "\x1b[0m"
-        red = '\x1b[31m'
-        bold_red = '\x1b[1;31m'
-        yellow = '\x1b[33m'
-        gray = '\x1b[2;37m'
+    # add custom levels
+    def add_log_level(name: str):
+        level = getattr(logging, name.upper())
+        logging.addLevelName(level, name.upper())
+        def f(self, message, *args, **kws):
+            if self.isEnabledFor(level):
+                self._log(level, message, args, **kws) 
+        setattr(logging.getLoggerClass(), name.lower(), f)
 
+    add_log_level('TRACE')
+    add_log_level('SUCCESS')
+
+    class ColoredFormatter(logging.Formatter):
         def __init__(self, fmt, colored):
             super().__init__()
+
+            # ANSI ECMA-48 color codes (semicolon-separated):
+            # bold=1, half-bright=2, underscore=4, doubly-underlined=21, normal-intensity=22
+            # foreground:30+color, background:40+color
+            # colors: black=0, red=1, green=2, brown=3, blue=4, magenta=5, cyan=6, white=7
+            no_change = lambda s: s
+            red = lambda s: f'\x1b[31m{s}\x1b[0m'
+            bold_red = lambda s: f'\x1b[1;31m{s}\x1b[0m'
+            bold_green = lambda s: f'\x1b[1;32m{s}\x1b[0m'
+            yellow = lambda s: f'\x1b[33m{s}\x1b[0m'
+            gray = lambda s: f'\x1b[2;37m{s}\x1b[0m'
+            cyan = lambda s: f'\x1b[36m{s}\x1b[0m'
+
             self.fmt = fmt
-            self.formats = {
-                logging.DEBUG: self.gray + fmt + self.reset,
-                logging.INFO: fmt,
-                logging.WARNING: self.yellow + fmt + self.reset,
-                logging.ERROR: self.red + fmt + self.reset,
-                logging.CRITICAL: self.bold_red + fmt + self.reset,
-            } if colored else {}
+            colors = {logging.TRACE: cyan, logging.DEBUG: gray, logging.INFO: no_change, logging.WARNING: yellow, logging.ERROR: red, logging.CRITICAL: bold_red, logging.SUCCESS: bold_green}
+            self.formats = {k: v(fmt) for k, v in colors.items()} if colored else {}
 
         def format(self, record):
             fmt = self.formats.get(record.levelno, self.fmt)
@@ -74,7 +88,7 @@ def get_parser():
 
     parser = argparse.ArgumentParser(description='program description')
     parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {__version__}')
-    parser.add_argument('--log-level', choices=['crit', 'error', 'warn', 'info', 'debug', 'none'], default='info', help='log level')
+    parser.add_argument('--log-level', choices=LOG_LEVELS.keys(), default='info', help='log level')
     parser.add_argument('--no-color', action='store_true', help='disables log coloring')
     return parser
 
@@ -86,6 +100,13 @@ def main(args):
     logger = get_logger(LOG_LEVELS[args.log_level], not args.no_color)
 
     # main logic
+    logger.trace('trace log')
+    logger.debug('debug log')
+    logger.info('info log')
+    logger.warning('warning log')
+    logger.error('error log')
+    logger.critical('critical log')
+    logger.success('success log')
 
 
 if __name__ == '__main__':
